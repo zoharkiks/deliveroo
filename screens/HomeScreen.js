8591f59459967337a6da2bad0718d503c5e96162
Zohar <zoharkiks@gmail.com>
2022-11-03 11:51:06 +0530

    Initial setup

---
+++ screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import { View, Text } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const HomeScreen = () => {
+
+const navigation = useNavigation()
+
+useLayoutEffect(() => {
+  navigation.setOptions({
+    headerShown:false
+  })
+
+ 
+}, [])
+
+  return (
+
+
+    <SafeAreaView>
+      <Text >HomeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
