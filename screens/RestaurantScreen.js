4e86458ed0321b515fc73722a279a6837fbd6cd3
Zohar <zoharkiks@gmail.com>
2022-11-28 12:06:53 +0530

    Add dish row

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -9,6 +9,7 @@ import {
   StarIcon,
 } from "react-native-heroicons/solid";
 import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
+import DishRow from "../components/DishRow";
 
 const RestaurantScreen = () => {
   const {
@@ -71,12 +72,34 @@ const RestaurantScreen = () => {
           <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
         </View>
 
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300  ">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="pl-2 flex-1 text-md font-bold ">
+            Have a food allergy?
+          </Text>
+          <ChevronRightIcon color="#00ccbb" />
+        </TouchableOpacity>
+      </View>
 
- <TouchableOpacity className='flex-row items-center space-x-2 p-4 border-y border-gray-300  '>
-    <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20}/>
-    <Text className='pl-2 flex-1 text-md font-bold '>Have a food allergy?</Text>
-    <ChevronRightIcon color="#00ccbb" />
- </TouchableOpacity>
+      <View >
+        <Text className='px-4 pt-6 mb-3 font-bold text-xl'>
+          Menu
+        </Text>
+
+{dishes.map(dish=>(
+  <DishRow
+  key={dish._id}
+  id={dish._id}
+  name={dish.name}
+  description={dish.short_description}
+  price={dish.price}
+  image={dish.image}
+  
+  
+
+
+  />
+))}
 
       </View>
     </ScrollView>
