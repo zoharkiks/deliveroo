5256bf1f4d60f4e7dac9d5e1a5044d47a5c3498a
Zohar <zoharkiks@gmail.com>
2022-11-30 12:14:13 +0530

    Finish basket screen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,46 +1,136 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { useDispatch, useSelector } from 'react-redux'
-import { selectRestaurant } from '../features/restaurantSlice'
-import { selectBasketItems} from '../features/basketSlice'
-import { XCircleIcon } from 'react-native-heroicons/solid'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRestaurant } from "../features/restaurantSlice";
+import { removeFromBasket, selectBasketItems, selectBasketTotal } from "../features/basketSlice";
+import { XCircleIcon } from "react-native-heroicons/solid";
+import { urlFor } from "../sanity";
+import Currency from "react-currency-formatter";
+
 
 const BasketScreen = () => {
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const basketTotal = useSelector(selectBasketTotal)
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
+  const dispatch = useDispatch();
 
-    const navigation = useNavigation()
-    const restaurant = useSelector(selectRestaurant)
-    const items = useSelector(selectBasketItems)
-    const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([])
-    const dispatch= useDispatch()
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
 
-    useEffect(() =>{
-        const groupedItems = items.reduce((results,item)=>{
-(results[item.id]= results[item.id]||[]).push(item)
-return results
-        },{})
-
-        setGroupedItemsInBasket(groupedItems)
-    } , [items])
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
 
   return (
-    <SafeAreaView className='flex-1 bg-white' >
-      <View className='flex-1 bg-gray-100'>
-        <View className='p-5 border-b border-[#00ccbb] bg-white shadow-xs'>
-            <View>
-                <Text className='text-lg font-bold text-center '>Basket</Text>
-                <Text className='text-center text-gray-400'>{restaurant.title}</Text>
-            </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00ccbb] bg-white shadow-xs">
+          <View>
+            <Text className="text-lg font-bold text-center ">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
 
-            <TouchableOpacity onPress={navigation.goBack} className='absolute bg-gray-100 rounded-full top-3 right-5'>
-                <XCircleIcon color='#00ccbb' height={50} width={50}/>
-            </TouchableOpacity>
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="absolute bg-gray-100 rounded-full top-3 right-5"
+          >
+            <XCircleIcon color="#00ccbb" height={50} width={50} />
+          </TouchableOpacity>
         </View>
 
-        
+        <View className="flex-row items-center px-4 py-3 my-5 space-x-4 bg-white">
+          <Image
+            source={{
+              uri: "https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2021/07/hd-1200-2.png?w=780&h=408&crop=1",
+            }}
+            className="w-10 h-10 p-4 bg-gray-300 rounded-full"
+          />
+          <Text className="flex-1">Deliver in 50- 54 mins</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00ccbb]">Change</Text>
+          </TouchableOpacity>
+        </View>
+
+        <ScrollView className='divide-y divide-gray-200'>
+          {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+            <View key={key} className='flex-row items-center px-5 py-2 space-x-3 bg-white'>
+              <Text className='text-[#00ccbb]'>{items.length} x</Text>
+              <Image
+                source={{
+                  uri: urlFor(items[0]?.image).url(),
+                }}
+                className='w-12 h-12 rounded-full'
+              />
+              <Text className='flex-1'>{items[0]?.name}</Text>
+
+              <Text className='text-gray-600'>
+                <Currency
+                quantity={items[0]?.price}
+                currency='INR'
+                />
+              </Text>
+
+              <TouchableOpacity>
+                <Text className='text-[#00ccbb] text-xs' onPress={()=> dispatch(removeFromBasket({id:key}))}>
+                  Remove
+                </Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+
+        <View className='p-5 mt-5 space-y-4 bg-white'>
+          <View className='flex-row justify-between'> 
+            <Text className='text-gray-400'>Subtotal</Text>
+            <Text className='text-gray-400'>
+            <Currency
+                quantity={basketTotal}
+                currency='INR'
+                />
+            </Text>
+          </View>
+
+          <View className='flex-row justify-between'> 
+            <Text className='text-gray-400'>Delivery</Text>
+            <Text className='text-gray-400'>
+            <Currency
+                quantity={40}
+                currency='INR'
+                />
+            </Text>
+          </View>
+
+          <View className='flex-row justify-between'> 
+            <Text >Order Total</Text>
+            <Text  className='font-extrabold '>
+            <Currency
+                quantity={basketTotal+40}
+                currency='INR'
+                />
+            </Text>
+          </View>
+          
+          <TouchableOpacity className='rounded-lg bg-[#00ccbb] p-4'>
+            <Text className='text-lg font-bold text-center text-white'>Place Order</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default BasketScreen
+export default BasketScreen;
